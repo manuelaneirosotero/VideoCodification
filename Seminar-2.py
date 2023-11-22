730ade2999fdb0d3492081c185e56979f9aa9607
Manuel Aneiros <manuelsalvador.aneiros01@estudiant.upf.edu>
2023-11-22 22:59:13 +0100

    added exercice 2 and 3 using ffmpeg

--- Seminar-2.py
+++ Seminar-2.py
@@ -17,30 +17,94 @@ ffmpeg_cmd = [
 # Run the FFmpeg command
 subprocess.run(ffmpeg_cmd)
 
-###############################################EX3##############################################################
+##################################################EX2###############################################
+# exercise 2
 
-#Counts the number of tracks in the mp4
-def count_tracks(mp4_file):
-    try:
-        video_clip = VideoFileClip(mp4_file)
+def create_bbb_container(self):
+    # Output file paths
+    output_video_file = 'bbb_50s.mp4'
+    output_mono_audio_file = 'bbb_mono.mp3'
+    output_stereo_low_bitrate_audio_file = 'bbb_stereo_low_bitrate.mp3'
+    output_aac_audio_file = 'bbb_aac.aac'
 
-        # Counting audio tracks
-        if video_clip.audio:
-            num_audio_tracks = len(video_clip.audio.reader.infos)
-        else:
-            num_audio_tracks = 0
+    # FFmpeg command to cut video to 50 seconds
+    cut_video_cmd = [
+        'ffmpeg',
+        '-i', self.input_file,
+        '-t', '50',
+        '-c', 'copy',
+        output_video_file
+    ]
 
-        # Counting video tracks
-        num_video_tracks = len(video_clip.reader.infos)
+    #FFmpeg command to export audio as MP3 mono track
+    export_mono_audio_cmd = [
+        'ffmpeg',
+        '-i', output_video_file,
+        '-vn',
+        '-ac', '1',
+        '-q:a', '2',
+        output_mono_audio_file
+    ]
 
-        print(f"The MP4 container contains {num_audio_tracks} audio track(s) and {num_video_tracks} video track(s).")
-    except Exception as e:
-        print(f"Error: {e}")
+    # FFmpeg command to export audio in MP3 stereo with lower bitrate
+    export_stereo_low_bitrate_audio_cmd = [
+        'ffmpeg',
+        '-i', output_video_file,
+        '-vn',
+        '-q:a', '5',
+        '-ac', '2',
+        output_stereo_low_bitrate_audio_file
+    ]
 
+    # FFmpeg command to export audio in AAC codec
+    export_aac_audio_cmd = [
+        'ffmpeg',
+        '-i', output_video_file,
+        '-vn',
+        '-c:a', 'aac',
+        output_aac_audio_file
+    ]
 
-# Example usage
-mp4_file_path = "BBB.mp4"
-count_tracks(mp4_file_path)
+    # FFmpeg command to package everything in a .mp4
+    package_cmd = [
+        'ffmpeg',
+        '-i', output_video_file,
+        '-i', output_mono_audio_file,
+        '-i', output_stereo_low_bitrate_audio_file,
+        '-i', output_aac_audio_file,
+        '-map', '0:v',
+        '-map', '1:a',
+        '-map', '2:a',
+        '-map', '3:a',
+        '-c', 'copy',
+        self.output_file
+    ]
+
+    # Run FFmpeg commands
+    subprocess.run(cut_video_cmd)
+    subprocess.run(export_mono_audio_cmd)
+    subprocess.run(export_stereo_low_bitrate_audio_cmd)
+    subprocess.run(export_aac_audio_cmd)
+    subprocess.run(package_cmd)
+
+##################################################EX3###############################################
+def get_track_count(self):
+    # FFprobe command to get information about input file
+    ffprobe_cmd = [
+        'ffprobe',
+        '-v', 'error',
+        '-select_streams', 'a:video',
+        '-count_frames', '-show_entries',
+        'stream=nb_read_frames',
+        '-of', 'default=nokey=1:noprint_wrappers=1',
+        input_video
+    ]
+
+    # Run FFprobe command and capture the output
+    result = subprocess.run(ffprobe_cmd, capture_output=True, text=True)
+    track_count = len(result.stdout.strip().split('\n'))
+    return track_count
+
 
 ###############################EX6########################################
 import subprocess
@@ -55,9 +119,11 @@ def create_video_with_histogram(input_video, histogram, output_video):
     subprocess.run(['ffmpeg', '-i', input_video, '-i', histogram, '-filter_complex', 'overlay=W-w-10:H-h-10', '-y', output_video])
 
 if __name__ == "__main__":
+
     input_video = "BBB.mp4"
     output_histogram = "/home/manu/Documents/VideoCodification"
     output_video = "BBB-histogram.mp4"
+    output_file = 'final_output.mp4'
 
     # Extract YUV histogram
     extract_yuv_histogram(input_video, output_histogram)
